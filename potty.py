4ef5fe325643652474abb0c85fc97447ac7fd048
Ulises Gonzalez <ulisesgz@outlook.com>
2015-06-15 16:26:53 -0600

    P.O.T.T.Y.
    código fuente

---
+++ potty.py
@@ -0,0 +1,21 @@
+import ply.lex as lex 
+import ply.yacc as yacc
+
+reservadas = {
+	'jugar' : 'jugar'
+	'dormir' : 'dormir'
+	'dar' : 'dar'
+	'vueltas' : 'vueltas'
+	'si' : 'si'
+	'entonces' : 'entonces'
+	'haga' : 'haga'
+	'mientras' : 'mientras'
+	'escribir' : 'escrbir'
+	'verdad' : 'verdad'
+	'mentira' : 'mentira'
+	'y' : 'y'
+	'o' : 'o'
+		
+}
+
+tokens = ['+', '-', '/'. '*', '(', ')', '{', '}', 'es', 'mayorque', 'menorque', 'iguala',  ]
