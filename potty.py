809795b7b0c2e1d67335d92ea0479538b53f65a2
Ulises González Zúñiga <ulisesgz@outlook.com>
2015-06-16 01:24:25 -0600

    trabajando tokens

--- potty.py
+++ potty.py
@@ -2,30 +2,30 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 reservadas = {
-	'jugar' : 'jugar'
-	'dormir' : 'dormir'
-	'dar' : 'dar'
-	'vueltas' : 'vueltas'
-	'si' : 'si'
-	'entonces' : 'entonces'
-	'haga' : 'haga'
-	'mientras' : 'mientras'
-	'escribir' : 'escrbir'
-	'verdad' : 'verdad'
-	'mentira' : 'mentira'
-	'y' : 'y'
-	'o' : 'o'
+	'JUGAR' : 'JUGAR'
+	'DORMIR' : 'DORMIR'
+	'DAR' : 'DAR'
+	'VUELTAS' : 'VUELTAS'
+	'SI' : 'SI'
+	'ENTONCES' : 'ENTONCES'
+	'HAGA' : 'HAGA'
+	'MIENTRAS' : 'MIENTRAS'
+	'ESCRIBIR' : 'ESCRIBIR'
+	'VERDAD' : 'VERDAD'
+	'MENTIRA' : 'MENTIRA'
+	'Y' : 'Y'
+	'O' : 'O'
 		
 }
 
-tokens = ['+', '-', '/'. '*', '(', ')', '{', '}', 'es', 'mayor_que', 'menor_que', 'igual_a', 'mayor_igual', 'menor_igual', 'no_igual', 'cadena', 'numero', 'letra'] + list(reservadas.values())
+tokens = ['+', '-', '/', '*', '(', ')', '{', '}', 'es', 'mayor_que', 'menor_que', 'igual_a', 'mayor_igual', 'menor_igual', 'diferente_a', 'variable', 'numero', 'letra', 'FINDELINEA', 'IDENTIFICADOR'] + list(reservadas.values())
 
 t_+ = r'\+'
 t_- = r'-'
 t_* = r'\*'
 t_/ = r'/'
 t_( = r'\('
-t_) = r')'
+t_) = r'\)'
 t_{ = r'{'
 t_} = r'}'
 t_es = r'='
@@ -34,5 +34,25 @@ t_menor_que = r'<'
 t_mayor_igual = r'>='
 t_menor_igual = r'<='
 t_igual_a = r'=='
-t_no_igual : r'!='
-t_cadena 
+t_diferente_a = r'!='
+variable = r'([a-z]+[A-Z]*[0-9]*)+'
+	
+def t_numero(t):
+	r'[0-9]+'
+	t.value = int(t.value)
+	return t
+	
+def t_FINDELINEA(t):
+    r'(\n)+'
+    t.lexer.lineno += t.value.count("\n")
+    return t
+
+def t_
+
+t_ignore = " \t"
+
+def t_IDENTIFICADOR(t):
+    r'[A-Z][A-Z0-9]*'
+    t.type = reservadas.get(t.value, 'IDENTIFICADOR')    # Check for reserved words
+    return t
+	
