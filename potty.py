2db229b2613f8780d4f319c1aaff8bcc774f52b3
Ulises Gonzalez <ulisesgz@outlook.com>
2015-06-18 09:00:23 -0600

    lex bien hecho

--- potty.py
+++ potty.py
@@ -15,29 +15,29 @@ reservadas = {
 	'MENTIRA' : 'MENTIRA'
 	'Y' : 'Y'
 	'O' : 'O'
-		
+
 }
 
-tokens = ['+', '-', '/', '*', '(', ')', '{', '}', 'es', 'mayor_que', 'menor_que', 'igual_a', 'mayor_igual', 'menor_igual', 'diferente_a', 'variable', 'numero', 'letra', 'FINDELINEA', 'IDENTIFICADOR'] + list(reservadas.values())
+tokens = ['SUMA', 'RESTA', 'DIV', 'MULT', 'PRI', 'PRD', 'LI', 'LD', 'ASIG', 'MAYQ', 'MENQ', 'IGUAL', 'MAYIGUAL', 'MENIGUAL', 'DIFERENTE', 'VARIABLE', 'NUM', 'LETRA', 'FINDELINEA', 'ID'] + list(reservadas.values())
 
-t_+ = r'\+'
-t_- = r'-'
-t_* = r'\*'
-t_/ = r'/'
-t_( = r'\('
-t_) = r'\)'
-t_{ = r'{'
-t_} = r'}'
-t_es = r'='
-t_mayor_que = r'>'
-t_menor_que = r'<'
-t_mayor_igual = r'>='
-t_menor_igual = r'<='
-t_igual_a = r'=='
-t_diferente_a = r'!='
-variable = r'([a-z]+[A-Z]*[0-9]*)+'
+t_SUMA = r'\+'
+t_RESTA = r'-'
+t_MULT = r'\*'
+t_DIV = r'/'
+t_PRI = r'\('
+t_PRD = r'\)'
+t_LI = r'{'
+t_LD = r'}'
+t_ASIG = r'='
+t_MAYQ = r'>'
+t_MENQ = r'<'
+t_MAYIGUAL = r'>='
+t_MENIGUAL = r'<='
+t_IGUAL= r'=='
+t_DIFERENTE = r'!='
+t_VARIABLE = r'([a-z]+[A-Z]*[0-9]*)+'
 	
-def t_numero(t):
+def t_NUM(t):
 	r'[0-9]+'
 	t.value = int(t.value)
 	return t
@@ -47,12 +47,26 @@ def t_FINDELINEA(t):
     t.lexer.lineno += t.value.count("\n")
     return t
 
-def t_
+#función que determina el número de línea 
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-t_ignore = " \t"
-
-def t_IDENTIFICADOR(t):
+def t_ID(t):
     r'[A-Z][A-Z0-9]*'
-    t.type = reservadas.get(t.value, 'IDENTIFICADOR')    # Check for reserved words
+    t.type = reservadas.get(t.value, 'ID')    # Check for reserved words
     return t
 	
+def t_error(t):
+    print 'Caracter ilegal'
+    t.lexer.skip(1)
+	
+t_ignore = " \t"
+
+lexer = ply.lex.lex()
+lexer.input("JUGAR SI 6 > 7")
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print tok
